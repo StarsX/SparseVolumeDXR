f9252cc898bef733770ac4e81948885ad26c2873
StarsX <X-Universe@outlook.com>
2020-11-22 17:28:08 +0800

    Remove redundant hears.

--- SparseVolumeDXR/stdafx.h
+++ SparseVolumeDXR/stdafx.h
@@ -30,13 +30,16 @@
 
 #include <iostream>
 #include <sstream>
-#include <codecvt>
 #include <iomanip>
 
+#if _HAS_CXX17
+#include <winrt/base.h>
+#else
 #include <algorithm>
 #include <string>
 #include <vector>
 #include <unordered_map>
+#endif
 #include <functional>
 #include <wrl.h>
 #include <shellapi.h>
